50ae81c21c50ea71bdc04f6f6c6ed8ec06edff0a
Lenin CK <leninchek@gmail.com>
2026-02-10 02:05:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1295545300-34dea",
+  "appId": "1:466919260891:web:98eabb0946dd30233c02fe",
+  "apiKey": "AIzaSyCRDpe38yVnbbDnHSFbirZ2XLBLPmbPg8s",
+  "authDomain": "studio-1295545300-34dea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "466919260891"
+};
